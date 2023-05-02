4c2acdc3d15d30a5ebd0b62afdf11ce42f1da6ef
Senglee Choi <tmdfl1027@gmail.com>
2023-05-02 12:29:36 +0900

    [Feat] echo 서버로 구현 변경
    최대 5개 클라이언트까지 연결 가능한 iterative 방식의 echo 서버로 구현 변경.

--- hello_server.c
+++ hello_server.c
@@ -5,20 +5,21 @@
 #include <arpa/inet.h>
 #include <sys/socket.h>
 
+#define BUF_SIZE 1024
+
 void error_handling(char* message);
 
 int main(int argc, char* argv[]) {
     int serv_sock;
     int clnt_sock;
-    int sent_len = 0;
 
     struct sockaddr_in serv_addr;
     struct sockaddr_in clnt_addr;
     socklen_t clnt_addr_size;
 
-    int writeIndex;
-
-    char message[] = "this is a very long message";
+    char message[BUF_SIZE];
+    int str_len;
+    int i;
 
     if (argc != 2) {
         printf("Usage : %s <port>\n", argv[0]);
@@ -45,17 +46,25 @@ int main(int argc, char* argv[]) {
     }
 
     clnt_addr_size = sizeof(clnt_addr);
-    clnt_sock = accept(serv_sock, (struct sockaddr*) &clnt_addr, &clnt_addr_size);
-    if (clnt_sock == -1) {
-        error_handling("accept() error");
+    
+    for (i = 0; i < 5; i++) {
+        clnt_sock = accept(serv_sock, (struct sockaddr*) &clnt_addr, clnt_addr_size);
+
+        if (clnt_sock == -1) {
+            error_handling("accept() error!");
+        } else {
+            printf("Conncected Client: %d\n", i + 1);
+        }
+
+        while ((str_len = read(clnt_sock, message, BUF_SIZE) != 0)) {
+            write(clnt_sock, message, str_len);
+        }
+
+        close(clnt_sock);
     }
 
-    write(clnt_sock, message, 10);
-    write(clnt_sock, message + 10, 10);
-    write(clnt_sock, message + 20, 8);
-
-    close(clnt_sock);
     close(serv_sock);
+
     return 0;
 }
 
